7cdbb192c8a7cf4f91c5129f023e374095d414e9
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2024-10-03 16:48:46 +0900

    랜덤손 출력

--- Drill-06.py
+++ Drill-06.py
@@ -1,12 +1,20 @@
 from pico2d import *
 import random
+
+
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-hand=load_image('hand_arrow')
+hand=load_image('hand_arrow.png')
 
-randomPoints=[(random.randint(-500,500),random.randint(-350,350))]
+
+running=True
+frame=0
+dirx=0
+diry=0
+randomPoints_X=random.randint(0,TUK_WIDTH)
+randomPoints_Y=random.randint(0,TUK_HEIGHT)
 
 def handl_events():
     pass
@@ -17,3 +25,11 @@ def moving_left():
     pass
 def moving_up():
     pass
+def draw_hand():
+    pass
+
+while True:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    hand.draw(randomPoints_X,randomPoints_Y)
+    update_canvas()
--- drill06.py
+++
@@ -1,5 +0,0 @@
-
-TUK_WIDTH, TUK_HEIGHT = 1280, 1024
-open_canvas(TUK_WIDTH, TUK_HEIGHT)
-tuk_ground = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.png')
