75591d826120dad67a3b38d3acc135a7d740a611
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2024-10-03 17:07:34 +0900

    핸들 이벤트 함수 뼈대 만들기

--- Drill-06.py
+++ Drill-06.py
@@ -11,12 +11,14 @@ hand=load_image('hand_arrow.png')
 
 running=True
 frame=0
+x=TUK_WIDTH//2
+y=TUK_HEIGHT//2
 dirx=0
 diry=0
 randomPoints_X=random.randint(0,TUK_WIDTH)
 randomPoints_Y=random.randint(0,TUK_HEIGHT)
 
-def handl_events():
+def handle_events():
     pass
 
 def moving_right():
@@ -25,11 +27,33 @@ def moving_left():
     pass
 def moving_up():
     pass
-def draw_hand():
-    pass
+def draw_right():
+    global frame
+    global x
+    x += dirx * 5
+    if x>790:
+        x=790
+    frame = (frame + 1) % 4
+    character.clip_draw(frame * 160, 4 + 640, 160, 160, x, y, 100, 100)
+    update_canvas()
 
-while True:
+def draw_left():
+    global frame
+    global x
+    x += dirx * 5
+    if x<10:
+        x=10
+    frame = (frame + 1) % 4
+    character.clip_draw(frame * 160, 4 + 320, 160, 160, x, y, 100, 100)
+
+def draw_hand():
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     hand.draw(randomPoints_X,randomPoints_Y)
     update_canvas()
+
+while True:
+    draw_hand()
+    draw_right()
+
+    delay(0.05)
